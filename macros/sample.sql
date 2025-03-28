7f3602888568f472b558a7018a7723755a1c5205
Uday Sivadi <Uday.Reddy@xlparts.com>
2025-03-28 11:36:28 +0000

    typo

--- macros/sample.sql
+++ macros/sample.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 {% macro run_stored_procedure(proc_name) %}
     % do log("Executing stored procedure: " ~ proc_name, info=True) %}
     {% set sql_statement = "CALL " ~ proc_name ~ "('XLP');" %}
